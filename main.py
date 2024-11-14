70994500ab7714c7f176320320f5c7733b5100a3
Mkonu13 <michael.konu@yahoo.com>
2024-11-14 05:12:08 -0500

    got the frankenstein texts and got started on the python script

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    with open("/Users/michaelkonu/workspace2/github.com/KingKonu13/bookbot/books/frankenstein.txt") as f:
+        file_content = f.read()
+    print(file_content) 
+
+main()
