9eb80b46b3f20d4c9830cd0cda1a8c8a60b019d1
Mkonu13 <michael.konu@yahoo.com>
2024-11-15 02:48:03 -0500

    last commit of bookbot

--- main.py
+++ main.py
@@ -7,6 +7,29 @@ def main():
         list_of_words_in_book = book.split()
         return len(list_of_words_in_book)
         
-    print(counts_number_of_words_in_books(file_content))
+    # print(counts_number_of_words_in_books(file_content))
+    
+    def chars_counter(text):
+        table_of_chars = {}
+        for char in text:
+            char = char.lower()
+            if char in table_of_chars:
+                table_of_chars[char] += 1 
+            else:
+                table_of_chars[char] = 1
+        return table_of_chars
+        
 
+    print(chars_counter(file_content))
+    
+    def report_generator(words, table):
+        print("--- Begin report of books/frankenstein.txt ---")
+        print(f"{words} words found in the document")
+        print()
+
+        for key,value in table.items():
+            print(f"The {key} charachter was found {value} times")
+        print("--- End report ---")
+        
+    report_generator(counts_number_of_words_in_books(file_content), chars_counter(file_content))
 main()
