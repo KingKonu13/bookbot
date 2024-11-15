270e832c5eebc166e344b4e37d049c57e63a57f9
Mkonu13 <michael.konu@yahoo.com>
2024-11-15 01:42:15 -0500

    wrote the word counter function

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     with open("/Users/michaelkonu/workspace2/github.com/KingKonu13/bookbot/books/frankenstein.txt") as f:
         file_content = f.read()
-    print(file_content) 
+    counter = 0 
+    
+    def counts_number_of_words_in_books(book):
+        list_of_words_in_book = book.split()
+        return len(list_of_words_in_book)
+        
+    print(counts_number_of_words_in_books(file_content))
 
 main()
